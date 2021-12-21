d33068bdd497043dacb804ca234392478c5bd6c3
Agnes <agnes.guillet@hotmail.fr>
2021-12-21 12:03:00 +0100

    ex2

---
+++ pdf/ex2/main.js
@@ -0,0 +1,33 @@
+// Ecrire une fonction en javascript ChangerTexte() qui permet de changer le contenu de la
+// balise <p> par le texte « Salut iTeam ! ».
+// 2. Ajouter un événement JavaScript pour que la fonction ChangerTexte() s’exécute en
+// cliquant sur le bouton
+
+let p;
+let btn;
+
+/************************************************************************************/
+/* ******************************** Datas **********************************/
+/************************************************************************************/
+
+/************************************************************************************/
+/* ******************************** Fonctions **********************************/
+/************************************************************************************/
+
+
+/************************************************************************************/
+/* ******************************** CODE PRINCIPAL **********************************/
+/************************************************************************************/
+
+
+
+function main(){
+
+    p = document.querySelector('#p1');
+    btn = document.querySelector("input");
+
+
+    btn.addEventListener("click", onClickButton);
+
+}
+document.addEventListener("DOMContentLoaded", main);
